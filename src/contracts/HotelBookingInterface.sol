0c2e7f67ad7b05275380f56a4a30e3d6d3b2f382
Eager-Blockchain-Dev <112506679+01blockchainDev@users.noreply.github.com>
2022-09-21 23:26:17 -0400

    Update HotelBookingInterface.sol
    first step

--- src/contracts/HotelBookingInterface.sol
+++ src/contracts/HotelBookingInterface.sol
@@ -1,7 +1,3 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-interface HotelBookingInterface{
-    function getName(uint _index) external view returns(string memory);
-    function getId(uint _index) external view returns(uint);
-}
